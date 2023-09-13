483036b69b51557b8666562bafeadc6d578b11a7
Emmanuel <attahzuzu@gmail.com>
2023-09-13 06:37:20 +0100

    remove rewrites from  nextjs config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    async rewrites() {
-        return [
-          {
-            source: "/api/subscribe",
-            destination: "https://numiattah.pythonanywhere.com/subscribe/",
-          },
-        ];
-      },
+    // async rewrites() {
+    //     return [
+    //       {
+    //         source: "/api/subscribe",
+    //         destination: "https://numiattah.pythonanywhere.com/subscribe/",
+    //       },
+    //     ];
+    //   },
 }
 
 module.exports = nextConfig
